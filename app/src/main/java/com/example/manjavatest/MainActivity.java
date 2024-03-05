fb12b2d1a846b3efa0edb984ace0eb3495aab16f
Nehal Agrawal <AgrawalNehal@g.bettendorf.k12.ia.us>
2024-03-05 15:22:09 -0600

    Starting the userdb and looking into adding data etc

--- app/src/main/java/com/example/manjavatest/MainActivity.java
+++ app/src/main/java/com/example/manjavatest/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        UsersDB usersdb = new UsersDB(this);
+        usersdb.addUser("JohnDoe123", "Farmer4Life", "John", "Doe", "doejohn@johndeere.com", "1234567890");
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/manjavatest/UsersDB.java
@@ -0,0 +1,70 @@
+package com.example.manjavatest;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class UsersDB extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "UsersDB";
+    private static final int DB_VERSION = 1;
+    private static final String TABLE_NAME = "Users";
+
+    private static final String KEY_ID = "id";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_PASSWORD = "password";
+    private static final String KEY_FIRST_NAME = "first_name";
+    private static final String KEY_LAST_NAME = "last_name";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_PHONE_NO = "phone_no";
+
+
+    public UsersDB(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // CREATE TABLE Users ( id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, first_name TEXT, last_name TEXT, email TEXT, phone_no TEXT
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(" + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + KEY_USERNAME + " TEXT, " + KEY_PASSWORD + " TEXT, "
+                + KEY_FIRST_NAME + " TEXT, " + KEY_LAST_NAME
+                + " TEXT, " + KEY_EMAIL + " TEXT, " + KEY_PHONE_NO + " TEXT)");
+
+//        SQLiteDatabase dB = this.getWritableDatabase();
+//
+//        // query to insert
+//
+//        dB.close();
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(" DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+
+    }
+
+    public void addUser(String username, String pass, String firstN, String lastN, String email, String phoneNum) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_USERNAME, username);
+        values.put(KEY_PASSWORD, pass);
+        values.put(KEY_FIRST_NAME, firstN);
+        values.put(KEY_LAST_NAME, lastN);
+        values.put(KEY_EMAIL, email);
+        values.put(KEY_PHONE_NO, phoneNum);
+
+        db.insert(TABLE_NAME, null, values);
+
+
+        db.close();
+
+    }
+}
