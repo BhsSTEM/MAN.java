20565dd16b9e74a0df8062e4a27793f55e0905f9
Nehal <62080303+N2041@users.noreply.github.com>
2024-03-18 02:10:35 -0500

    Frontend stuff for login, not connected to db yet tho

--- app/src/main/java/com/example/manjavatest/MainActivity.java
+++ app/src/main/java/com/example/manjavatest/MainActivity.java
@@ -4,24 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    EditText user;
+    EditText pass;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        setContentView(R.layout.fragment_login);
 
         UsersDB usersdb = new UsersDB(this);
 //        usersdb.addUser("JohnDoe123", "Farmer4Life", "John", "Doe", "doejohn@johndeere.com", "1234567890");
 
-        ArrayList<ModelUser> data = usersdb.fetchUsers();
-        for (ModelUser mu : data) {
-        Log.d("User info", "Username " + mu.username + " Password " + mu.pass +
-                    " First Name " + mu.firstN + " Last Name " + mu.lastN +
-                    " Email " + mu.email + " Phone Number " + mu.phoneNum);
-        }
+//        ArrayList<ModelUser> data = usersdb.fetchUsers();
+//        for (ModelUser mu : data) {
+//        Log.d("User info", "Username " + mu.username + " Password " + mu.pass +
+//                    " First Name " + mu.firstN + " Last Name " + mu.lastN +
+//                    " Email " + mu.email + " Phone Number " + mu.phoneNum);
+//        }
+
+        user = findViewById(R.id.username);
+        pass = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(user.getText().toString().equals("JohnDoe") && pass.getText().toString().equals("Farmer4Life")) {
+                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
